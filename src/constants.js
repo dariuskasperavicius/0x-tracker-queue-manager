6889a0363a4637c23bd2b407740ba4ba130df36e
Craig Bovis <craig@bovis.me.uk>
2019-12-10 08:16:15 +0000

    Add bulk indexing job

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,13 @@
 const QUEUE = {
+  BULK_INDEXING: 'bulk-indexing',
   FILL_INDEXING: 'fill-indexing',
   FILL_PROCESSING: 'fill-processing',
 };
 
 const JOB = {
+  BULK_INDEX_FILLS: 'bulk-index-fills',
   FETCH_FILL_STATUS: 'fetch-fill-status',
   INDEX_FILL: 'index-fill',
-  REINDEX_FILLS: 'reindex-fills',
 };
 
 module.exports = { JOB, QUEUE };
