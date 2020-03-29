9b72e7ac710ce8a3574ff88241e0f47ae27362e3
Craig Bovis <craig@bovis.me.uk>
2020-03-29 14:52:50 -0500

    Sync jobs constant with worker (#27)

--- src/constants.js
+++ src/constants.js
@@ -6,8 +6,12 @@ const QUEUE = {
 
 const JOB = {
   BULK_INDEX_FILLS: 'bulk-index-fills',
+  CONVERT_PROTOCOL_FEE: 'convert-protocol-fee',
   FETCH_FILL_STATUS: 'fetch-fill-status',
   INDEX_FILL: 'index-fill',
+  INDEX_FILL_PROTOCOL_FEE: 'index-fill-protocol-fee',
+  INDEX_FILL_STATUS: 'index-fill-status',
+  INDEX_FILL_VALUE: 'index-fill-value',
 };
 
 module.exports = { JOB, QUEUE };
