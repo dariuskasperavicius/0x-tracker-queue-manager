4373d8ece9b144e15d63b6de87695ed0816363ef
Craig Bovis <craig@bovis.me.uk>
2020-03-29 19:18:50 -0500

    Update job & queue constants

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,15 @@ const QUEUE = {
   BULK_INDEXING: 'bulk-indexing',
   FILL_INDEXING: 'fill-indexing',
   FILL_PROCESSING: 'fill-processing',
+  TOKEN_PROCESSING: 'token-processing',
 };
 
 const JOB = {
   BULK_INDEX_FILLS: 'bulk-index-fills',
+  BULK_UPDATE_TOKEN_METADATA: 'bulk-update-token-metadata',
   CONVERT_PROTOCOL_FEE: 'convert-protocol-fee',
   FETCH_FILL_STATUS: 'fetch-fill-status',
+  FETCH_TOKEN_METADATA: 'fetch-token-metadata',
   INDEX_FILL: 'index-fill',
   INDEX_FILL_PROTOCOL_FEE: 'index-fill-protocol-fee',
   INDEX_FILL_STATUS: 'index-fill-status',
